c851fd7f9232757b591b99c4c34ec9b2bc2c379b
Khanhbq2k <khanhbui@mfunctions.com>
2023-05-27 11:15:03 +0700

    add gradle benchmark module

--- benchmark/src/main/java/com/ctriposs/sdb/benchmark/BigMapBenchmark.java
+++ benchmark/src/main/java/com/ctriposs/sdb/benchmark/BigMapBenchmark.java
@@ -8,61 +8,61 @@
 //
 //public class BigMapBenchmark extends DbBenchmark {
 //
-//	private final String databaseDir_;
-//	private static String DATABASE_NAME = "sample";
-//	private BigConcurrentHashMapImpl map_;
+//    private final String databaseDir_;
+//    private static String DATABASE_NAME = "sample";
+//    private BigConcurrentHashMapImpl map_;
 //
-//	public BigMapBenchmark(String[] commands) {
-//		super(commands);
-//		databaseDir_ = this.getBaseDir() + "bitmap/unit/benchmark";
-//		dbName = "BigMap";
-//		version = "1.0.0";
-//	}
+//    public BigMapBenchmark(String[] commands) {
+//        super(commands);
+//        databaseDir_ = this.getBaseDir() + "bitmap/unit/benchmark";
+//        dbName = "BigMap";
+//        version = "1.0.0";
+//    }
 //
-//	@Override
-//	public void open() {
-//		try {
-//			map_ = new BigConcurrentHashMapImpl(databaseDir_, DATABASE_NAME);
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
+//    @Override
+//    public void open() {
+//        try {
+//            map_ = new BigConcurrentHashMapImpl(databaseDir_, DATABASE_NAME);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 //
-//	@Override
-//	public void put(byte[] key, byte[] value) {
-//		map_.put(key, value);
+//    @Override
+//    public void put(byte[] key, byte[] value) {
+//        map_.put(key, value);
 //
-//	}
+//    }
 //
-//	@Override
-//	public byte[] get(byte[] key) {
-//		return map_.get(key);
-//	}
+//    @Override
+//    public byte[] get(byte[] key) {
+//        return map_.get(key);
+//    }
 //
-//	@Override
-//	public void close() {
-//		try {
-//			map_.close();
-//		} catch (IOException e) {
-//			e.printStackTrace();
-//		}
-//	}
+//    @Override
+//    public void close() {
+//        try {
+//            map_.close();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    }
 //
-//	@Override
-//	public void destroyDb() {
-//		if (map_ != null) {
-//			try {
-//				map_.close();
-//				FileUtil.deleteDirectory(new File(databaseDir_));
-//				map_ = null;
-//			} catch (IOException e) {
-//				e.printStackTrace();
-//			}
-//		}
-//	}
+//    @Override
+//    public void destroyDb() {
+//        if (map_ != null) {
+//            try {
+//                map_.close();
+//                FileUtil.deleteDirectory(new File(databaseDir_));
+//                map_ = null;
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
 //
-//	public static void main(String[] args) throws Exception {
-//		new BigMapBenchmark(args).run();
-//	}
+//    public static void main(String[] args) throws Exception {
+//        new BigMapBenchmark(args).run();
+//    }
 //
 //}
